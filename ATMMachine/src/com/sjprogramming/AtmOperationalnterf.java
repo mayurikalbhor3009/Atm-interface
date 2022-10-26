b9a4b631f5422064c9e8e08d11302b316ec0b50e
mayurikalbhor3009 <111555860+mayurikalbhor3009@users.noreply.github.com>
2022-10-26 18:15:35 +0530

    Add files via upload

---
+++ ATMMachine/src/com/sjprogramming/AtmOperationalnterf.java
@@ -0,0 +1,10 @@
+package com.sjprogramming;
+
+public interface AtmOperationalnterf {
+
+
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
